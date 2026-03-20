7e9fa5b0a048625cc3ddb3b1d9c9ae7f1ad0842e
rolandparnaud <roland.parnaud@gmail.com>
2026-03-20 14:16:03 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bhfowohsqsvfwnuwbmvh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoZm93b2hzcXN2ZndudXdibXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5OTc4MTgsImV4cCI6MjA4OTU3MzgxOH0.RLGhS3wNNnYYoGUUgHAU5s651XDDqTB54mge8dluqMY'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoZm93b2hzcXN2ZndudXdibXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5OTc4MTgsImV4cCI6MjA4OTU3MzgxOH0.RLGhS3wNNnYYoGUUgHAU5s651XDDqTB84mge8dluqMY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
